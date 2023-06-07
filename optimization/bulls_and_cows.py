f4703c8a4cfe32c24df41e9dab1bf489ee0ebe03
Dmitry Dryomov <dremov@tochka.com>
2023-06-07 14:28:53 +0700

    fix: updated logic to generate a possible number

--- optimization/bulls_and_cows.py
+++ optimization/bulls_and_cows.py
@@ -13,15 +13,27 @@ position_possible = np.ones(shape=(number_length, len(digits)), dtype=bool)
 position_possible[0, 0] = False
 
 def random_possible_number(correct_digits, position_possible):
-    guess_digits = []
+
+    possible_digits_for_position = []
     for position, digit in enumerate(correct_digits):
         if digit is None:
             possible_digits = np.nonzero(position_possible[position, :])[0]
-            possible_digits = [d for d in possible_digits if d not in guess_digits]
             print(position, possible_digits, file=sys.stderr)
-            digit = random.choice(possible_digits)
-        guess_digits.append(digit)
-    return guess_digits
+        else:
+            possible_digits = [digit]
+
+        possible_digits_for_position.append((position, possible_digits))
+
+    while True:
+        guess_digits = [None] * number_length
+        try:
+            for position, possible_digits in sorted(possible_digits_for_position, key=lambda x: len(x[1])):
+                possible_digits = [d for d in possible_digits if d not in guess_digits]
+                digit = random.choice(possible_digits)
+                guess_digits[position] = digit
+            return guess_digits
+        except IndexError:
+            pass
 
 def update_possible(correct_digits, position_possible, bulls, cows, guess_digits):
     # update possibilities:
@@ -30,8 +42,10 @@ def update_possible(correct_digits, position_possible, bulls, cows, guess_digits
     for position, digit in enumerate(guess_digits):
         if cows == 0 and bulls == 0:
             position_possible[:, digit] = False
+            print(f"Eliminate digit {digit}", file=sys.stderr)
         if bulls == 0:
             position_possible[position, digit] = False
+            print(f"Exclude digit {digit} position {position}", file=sys.stderr)            
 
 guess_digits = None
 while True:
