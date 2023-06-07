93a50b70a1c5c7af92fce021afa3b93dcaeec616
Dmitry Dryomov <dremov@tochka.com>
2023-06-07 14:19:32 +0700

    feat: simple approach to exclude impossible

--- optimization/bulls_and_cows.py
+++ optimization/bulls_and_cows.py
@@ -7,7 +7,7 @@ import random
 digits = list(range(10))
 number_length = int(input())
 correct_digits = [None] * number_length
-position_possible = np.ones(shape=(number_length, len(digits)), dtype=np.bool)
+position_possible = np.ones(shape=(number_length, len(digits)), dtype=bool)
 
 # Can't place "0" on first position
 position_possible[0, 0] = False
@@ -23,7 +23,20 @@ def random_possible_number(correct_digits, position_possible):
         guess_digits.append(digit)
     return guess_digits
 
+def update_possible(correct_digits, position_possible, bulls, cows, guess_digits):
+    # update possibilities:
+    # if cows and bulls count is zero all guess digits in all positions are impossible
+    # if bulls count is zero all guess digits in current positions are impossible
+    for position, digit in enumerate(guess_digits):
+        if cows == 0 and bulls == 0:
+            position_possible[:, digit] = False
+        if bulls == 0:
+            position_possible[position, digit] = False
+
+guess_digits = None
 while True:
     bulls, cows = [int(i) for i in input().split()]
+    if bulls != -1:
+        update_possible(correct_digits, position_possible, bulls, cows, guess_digits)
     guess_digits = random_possible_number(correct_digits, position_possible)
     print(''.join(map(str, guess_digits)))
